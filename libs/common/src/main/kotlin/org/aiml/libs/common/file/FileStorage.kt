468fe38034497487e545ed4fe7e60c3f59573f02
JunYoung An <anjnyng98@gmail.com>
2025-06-01 18:15:25 +0900

    add libs for fileStorage

--- libs/common/src/main/kotlin/org/aiml/libs/common/file/FileStorage.kt
+++ libs/common/src/main/kotlin/org/aiml/libs/common/file/FileStorage.kt
@@ -4,5 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 interface FileStorage {
   fun uploadFile(file: MultipartFile, path: String): String
+  fun deleteFile(path: String): Unit
   fun getUrl(path: String?): String
 }
